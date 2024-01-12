a28e7f1fd3f441759f061e7b2874ee499215f208
Yashashree Bedre <134850620+YashashreeBedre@users.noreply.github.com>
2024-01-12 20:40:48 +0530

    Add files via upload
    Intro and Hello World

---
+++ Basics.java
@@ -0,0 +1,12 @@
+package ApnaCollege;
+
+public class Basics {
+ public static void main(String[] args) {
+	 System.out.println("hello world \n");
+
+
+	 System.out.println("New Line");
+	 
+	 
+}
+}
