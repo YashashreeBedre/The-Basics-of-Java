8f9dd1ff14b6ccd6609f46ea5ff8c23f7234c75b
Yashashree Bedre <134850620+YashashreeBedre@users.noreply.github.com>
2024-01-12 20:43:39 +0530

    Add files via upload
    Taking input using Scanner Class

---
+++ ScanInput.java
@@ -0,0 +1,24 @@
+package ApnaCollege;
+
+import java.util.*;
+
+
+public class ScanInput {
+	 public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.println("enter a");
+		 
+		 int a = sc.nextInt();
+		 
+		 System.out.println(" enter b");
+		 int b = sc.nextInt();
+		 
+		 int c = a + b;
+		 
+		 System.out.println("Sum of A And B is " + c);
+		 
+	}
+	}
+
