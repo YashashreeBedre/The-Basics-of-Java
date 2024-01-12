642b3e16700810267cf7404450b84ff3baa50621
Yashashree Bedre <134850620+YashashreeBedre@users.noreply.github.com>
2024-01-12 20:46:16 +0530

    Add files via upload
    find out number is odd or even using the if-esle.

---
+++ OddEven.java
@@ -0,0 +1,22 @@
+package ApnaCollege;
+
+import java.util.*;
+public class OddEven {
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Enter the number");
+	int number = sc.nextInt();
+	
+	if ( number % 2 == 0) {
+		System.out.println(" even");
+	}
+	else {
+		System.out.println("odd");
+	}
+	
+		
+	}
+
+}
