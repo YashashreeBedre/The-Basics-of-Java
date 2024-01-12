57e9b793950e4f7b0de0b92888af5c8776e8b3e0
Yashashree Bedre <134850620+YashashreeBedre@users.noreply.github.com>
2024-01-12 20:45:10 +0530

    Add files via upload
    Conditional Statements Basics

---
+++ IfElse.java
@@ -0,0 +1,19 @@
+package ApnaCollege;
+
+import java.util.*;
+public class IfElse {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter the age");
+		 int age = sc.nextInt()
+;
+		 
+		 if( age > 18) {
+			 System.out.println("Adult");
+		 }
+		 else {
+			 System.out.println("not adult");
+		 }
+	}
+}
